cf7465dba8803962095ec91d9178ef749de0f977
Asiri Jayawardena <it22323934@my.sliit.lk>
2025-05-14 13:54:52 +0530

    feat: changed the config

--- country-explorer/cypress.config.js
+++ country-explorer/cypress.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  viewportWidth: 1280,
+  viewportHeight: 800,
+  defaultCommandTimeout: 5000,
   e2e: {
+    baseUrl: 'http://localhost:5173',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
